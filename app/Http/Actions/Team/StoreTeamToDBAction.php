e411345b871c23d0eb493aaa7635b91a644e8bac
Morteza Jamasb <jamaseb.morteza@gmail.com>
2021-10-30 18:15:46 +0330

    update teadm feature

--- app/Http/Actions/Team/StoreTeamToDBAction.php
+++ app/Http/Actions/Team/StoreTeamToDBAction.php
@@ -6,13 +6,16 @@ use App\Models\Team\Team;
 
 class StoreTeamToDBAction
 {
-    public static function handle(array $data)
+    public static function handle(array $data, $team_id = null)
     {
         $data = array_merge($data, [
             'user_id' => auth()->id(),
             'personal_team' => isset($data['personal_team']) && $data['personal_team'] == 1 ? 1 : 0,
         ]);
         unset($data['members']);
+        if ($team_id) {
+            return Team::where('id', $team_id)->limit(1)->update($data);
+        }
         return Team::create($data);
     }
 }
